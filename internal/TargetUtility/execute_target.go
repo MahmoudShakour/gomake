86bf9e6ee92c051a819f490127f403021b7b4d0d
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-10-11 17:33:09 +0300

    Finish implementing execute a specific target and its dependencies logic

--- internal/TargetUtility/execute_target.go
+++ internal/TargetUtility/execute_target.go
@@ -1,1 +1,63 @@
 package internal
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+
+	internal "github.com/MahmoudShakour/gomake.git/internal/parser"
+)
+
+
+func buildTopoSort(node int,visited []int,adjList map[int][]int) []int{
+	visited[node]=1
+	topoSort:=make([]int,0)
+	for _,to :=range adjList[node] {
+		if visited[to]==0{
+			topoSort=append(topoSort,buildTopoSort(to,visited,adjList)...)
+		}
+	}
+	topoSort=append(topoSort, node)
+	return topoSort
+}
+
+func ExecuteTarget(targets []internal.Target, targetId int,numberOfTargets int) error {
+	err := CheckInvalidDependency(targets)
+	if err != nil {
+		return err
+	}
+	err = CheckCyclicDependency(targets)
+	if err != nil {
+		return err
+	}
+	
+	targetIdToTargetCommand:=map[int]string {}
+	for _,target:=range targets {
+		targetIdToTargetCommand[target.Id]=target.Command
+	}
+
+	adjList:=BuildAdjList(targets)
+	visited:=make([]int,numberOfTargets)
+	topoSort:=buildTopoSort(targetId,visited,adjList)
+
+	
+	for _,targetId:=range topoSort {
+		targetCommand:=targetIdToTargetCommand[targetId]
+		output,err:=executeCommand(targetCommand)
+		if err!=nil {
+			return err	
+		}
+		fmt.Println(output)
+	}
+	return nil
+}
+
+
+func executeCommand(command string) (string,error){
+	splittedCommand:=strings.Split(command, " ")
+
+	cmd:=exec.Command(splittedCommand[0],splittedCommand[1:]...)
+	output,err:=cmd.CombinedOutput()
+
+	return string(output),err
+}
