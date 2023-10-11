7c315aeab0f320c560afbc1ed9912902eab87328
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-10-11 21:44:18 +0300

    Edit Execute target & Add tests

--- internal/TargetUtility/execute_target.go
+++ internal/TargetUtility/execute_target.go
@@ -1,63 +1,60 @@
 package internal
 
 import (
-	"fmt"
 	"os/exec"
 	"strings"
 
 	internal "github.com/MahmoudShakour/gomake.git/internal/parser"
 )
 
-
-func buildTopoSort(node int,visited []int,adjList map[int][]int) []int{
-	visited[node]=1
-	topoSort:=make([]int,0)
-	for _,to :=range adjList[node] {
-		if visited[to]==0{
-			topoSort=append(topoSort,buildTopoSort(to,visited,adjList)...)
+func BuildTopoSort(node int, visited []int, adjList map[int][]int) []int {
+	visited[node] = 1
+	topoSort := make([]int, 0)
+	for _, to := range adjList[node] {
+		if visited[to] == 0 {
+			topoSort = append(topoSort, BuildTopoSort(to, visited, adjList)...)
 		}
 	}
-	topoSort=append(topoSort, node)
+	topoSort = append(topoSort, node)
 	return topoSort
 }
 
-func ExecuteTarget(targets []internal.Target, targetId int,numberOfTargets int) error {
+func ExecuteTarget(targets []internal.Target, targetId int, numberOfTargets int) (string, error) {
 	err := CheckInvalidDependency(targets)
 	if err != nil {
-		return err
+		return "", err
 	}
 	err = CheckCyclicDependency(targets)
 	if err != nil {
-		return err
+		return "", err
+	}
+	targetIdToTargetCommand := map[int]string{}
+	for _, target := range targets {
+		targetIdToTargetCommand[target.Id] = target.Command
 	}
 	
-	targetIdToTargetCommand:=map[int]string {}
-	for _,target:=range targets {
-		targetIdToTargetCommand[target.Id]=target.Command
-	}
-
-	adjList:=BuildAdjList(targets)
-	visited:=make([]int,numberOfTargets)
-	topoSort:=buildTopoSort(targetId,visited,adjList)
-
+	adjList := BuildAdjList(targets)
+	visited := make([]int, numberOfTargets)
+	topoSort := BuildTopoSort(targetId, visited, adjList)
 	
-	for _,targetId:=range topoSort {
-		targetCommand:=targetIdToTargetCommand[targetId]
-		output,err:=executeCommand(targetCommand)
-		if err!=nil {
-			return err	
+	output := ""
+	for _, targetId := range topoSort {
+		targetCommand := targetIdToTargetCommand[targetId]
+		commandOutput, err := executeCommand(targetCommand)
+		if err != nil {
+			return "", err
+		} else if len(commandOutput) != 0 {
+			output += commandOutput
 		}
-		fmt.Println(output)
 	}
-	return nil
+	return output, nil
 }
 
+func executeCommand(command string) (string, error) {
+	splittedCommand := strings.Split(command, " ")
 
-func executeCommand(command string) (string,error){
-	splittedCommand:=strings.Split(command, " ")
+	cmd := exec.Command(splittedCommand[0], splittedCommand[1:]...)
+	output, err := cmd.CombinedOutput()
 
-	cmd:=exec.Command(splittedCommand[0],splittedCommand[1:]...)
-	output,err:=cmd.CombinedOutput()
-
-	return string(output),err
+	return string(output), err
 }
--- internal/TargetUtility/execute_target_test.go
+++ internal/TargetUtility/execute_target_test.go
@@ -1,1 +1,40 @@
 package internal
+
+import (
+	"reflect"
+	"testing"
+
+	internal "github.com/MahmoudShakour/gomake.git/internal/parser"
+)
+
+
+func TestBuildTopoSort(t *testing.T) {
+	
+	t.Run("target with no dependencies",func(t *testing.T) {
+		var targets = []internal.Target{
+			{Id: 0, Name: "build", Dependencies: []string{"clean"}, Command: "go build -o myapp main.go"},
+			{Id: 1, Name: "clean", Dependencies: []string{}, Command: "go clean"},
+			{Id: 2, Name: "run", Dependencies: []string{"build"}, Command: "./myapp"},
+		}
+		got:=BuildTopoSort(1,make([]int, 3),BuildAdjList(targets))
+		want:=[]int {1}
+
+		if  !reflect.DeepEqual(got,want) {
+			t.Errorf("got %v want %v",got,want)
+		}
+	})
+
+	t.Run("target with linear dependencies",func(t *testing.T) {
+		var targets = []internal.Target{
+			{Id: 0, Name: "build", Dependencies: []string{"clean"}, Command: "go build -o myapp main.go"},
+			{Id: 1, Name: "clean", Dependencies: []string{}, Command: "go clean"},
+			{Id: 2, Name: "run", Dependencies: []string{"build"}, Command: "./myapp"},
+		}
+		got:=BuildTopoSort(2,make([]int, 3),BuildAdjList(targets))
+		want:=[]int {1,0,2}
+
+		if  !reflect.DeepEqual(got,want) {
+			t.Errorf("got %v want %v",got,want)
+		}
+	})	
+}
