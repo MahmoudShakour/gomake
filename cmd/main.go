feebc00eb20e0868174faf79ef89a17035d3ee0d
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-10-11 21:44:42 +0300

    complete program main logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,54 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
-	internal "github.com/MahmoudShakour/gomake.git/internal/parser"
+	"os"
+
+	exec "github.com/MahmoudShakour/gomake.git/internal/TargetUtility"
+	parser "github.com/MahmoudShakour/gomake.git/internal/parser"
 )
 
-func getArguments() (string,string) {
-	makeFilePath:=flag.String("f","./makefile","path of the makefile")
-	target:=flag.String("t","all","target")
+var (
+	defaultFilePath   = "/home/mahmoudshakour/Workspace/gomake/makefile"
+	defaultTarget     = "run"
+	ErrTargetNotFound = errors.New("given target is not found")
+)
+
+func getArguments() (string, string) {
+	makeFilePath := flag.String("f", defaultFilePath, "")
+	target := flag.String("t", defaultTarget, "")
 	flag.Parse()
-	return *makeFilePath,*target
+	return *makeFilePath, *target
 }
 
-
+func getTargetId(targets []parser.Target, targetName string) (int, error) {
+	for _, target := range targets {
+		if target.Name == targetName {
+			return target.Id, nil
+		}
+	}
+	return -1, ErrTargetNotFound
+}
 func main() {
-	
-	
-	makeFilePath,_:=getArguments()
-	directoryName,fileName := internal.ParsePath(makeFilePath)
-	targets:=internal.ParseMakeFile(directoryName,fileName)
-	
-	
-	fmt.Println(targets)
-	
+
+	makeFilePath, target := getArguments()
+	directoryName, fileName := parser.ParsePath(makeFilePath)
+	targets := parser.ParseMakeFile(directoryName, fileName)
+	targetId, err := getTargetId(targets, target)
+
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	output, err := exec.ExecuteTarget(targets, targetId, len(targets))
+
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	} else {
+		fmt.Print(output)
+	}
 }
